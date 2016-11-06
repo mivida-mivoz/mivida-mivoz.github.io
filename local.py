a0292054a6773da40d8197471111254604479252
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-11-06 10:41:04 -0500

    add initial static files and templates

--- local.py
+++ local.py
@@ -17,17 +17,5 @@ def home():
     return render_template('index.html')
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Handles 404 error"""
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def server_error(e):
-    """Handles 500 error"""
-    return render_template('500.html'), 500
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
